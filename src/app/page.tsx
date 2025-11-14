10028ab928662160d906a97b28897d37ffd7839b
kaloyan_03 <kaloyan.tsvetkov03@gmail.com>
2025-11-14 11:22:24 +0200

    deployment fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <p>Welcome to the home page</p>
+    <section className="p-8">
+      <h1 className="text-3xl font-bold mb-2">About Me</h1>
+      <p className="text-gray-400">
+        I’m a frontend engineer with 3+ years of experience building modern web applications using React, Angular, and TypeScript.
+      </p>
+    </section>
   );
 }
