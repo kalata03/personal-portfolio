9a8cc7d541a61fd582f97d7112f79df8d22b783c
kaloyan_03 <kaloyan.tsvetkov03@gmail.com>
2025-11-14 11:25:14 +0200

    added initial template for the pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
 export default function Home() {
   return (
-    <section className="p-8">
-      <h1 className="text-3xl font-bold mb-2">About Me</h1>
-      <p className="text-gray-400">
-        I’m a frontend engineer with 3+ years of experience building modern web applications using React, Angular, and TypeScript.
-      </p>
-    </section>
+    <p>Welcome to home page</p>
   );
 }
